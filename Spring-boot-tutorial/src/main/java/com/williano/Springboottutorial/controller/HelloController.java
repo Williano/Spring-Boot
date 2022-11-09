7b5bb701e74bcca49eb6bc7165cb5fd689fa8dfe
w0k013m <william.kwabla@walmart.com>
2022-11-09 16:23:01 -0600

    Add new project

---
+++ Spring-boot-tutorial/src/main/java/com/williano/Springboottutorial/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.williano.Springboottutorial.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String HelloWorld() {
+        return "Hello World!";
+    }
+
+}
